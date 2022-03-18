a259243ac87e37636c5a053ff06f6c17aa623cb1
BaptisteZloch <bzloch@hotmail.fr>
2022-03-18 11:16:06 +0100

    init projet + gitignore

---
+++ crust_break_backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for crust_break_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crust_break_backend.settings')
+
+application = get_asgi_application()
