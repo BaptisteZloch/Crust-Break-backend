566d468bcabaaf0f142669dcb05c2d8ebc93852f
BaptisteZloch <bzloch@hotmail.fr>
2022-03-22 16:29:35 +0100

    add requirements.txt

--- crust_break_user_api/urls.py
+++ crust_break_user_api/urls.py
@@ -21,15 +21,15 @@ from django.conf.urls.static import static
 app_name = 'crust_break_user_api'
 urlpatterns = [
     path('add/user', views.AddUser,name="add_user"), #C
-    path('list', views.listUser,name="list_user"), #R
+    path('list', views.listUser,name="list_user"),   #R
     path('detail/<int:user_id>', views.detailUser,name="detail_user"), #R
     path('update/<int:user_id>', views.updateUser,name="update_user"), #U
     path('delete/<int:user_id>', views.deleteUser,name="delete_user"), #D
     
-    path('add/to-do/<int:recette_id>', views.AddRecetteToDo,name="add_user_todo_recette"),
+    path('<int:user_id>add/to-do/<int:recette_id>', views.AddRecetteToDo,name="add_user_todo_recette"),
     path('<int:user_id>/get/todo-receipes', views.getRecetteToDo,name="get_user_todo_recette"),
     
-    path('add/to-favorites/<int:recette_id>', views.AddRecetteToFavorites,name="add_user_favorites_recette"),
+    path('<int:user_id>add/to-favorites/<int:recette_id>', views.AddRecetteToFavorites,name="add_user_favorites_recette"),
     path('<int:user_id>/get/favorites-receipes', views.getRecetteFavorites,name="get_user_todo_recette"),
     #path('search', views.searchRecette,name="search_recette"),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
