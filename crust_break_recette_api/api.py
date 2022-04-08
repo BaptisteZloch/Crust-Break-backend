e84cc1f251302eeff8c34d6ce149078c9dcbb3ac
BaptisteZloch <bzloch@hotmail.fr>
2022-04-08 15:02:43 +0200

    search and details ok

--- crust_break_recette_api/api.py
+++ crust_break_recette_api/api.py
@@ -25,8 +25,17 @@ class Api():
             "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
             "X-RapidAPI-Key": settings.RAPIDAPI_KEY
         }
+        param_string = {
+            "query":query_dict['name'],#name of the food : burger, pizza...
+            "cuisine":query_dict['cuisine'],#name of the cuisine : american, african...
+            "type":query_dict['type'],#type of meal : main course, entry...
+            "diet":query_dict['diet'],#type of diet : paleo, primal, and vegetarian...
+            "excludeIngredients":query_dict['exclude'],# exemple : coconut, mango
+            "instructionsRequired":True,
+            "number":20,
+            }
         url ='https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex'
 
-        response = requests.get(url=url,headers=headers)
+        response = requests.get(url=url,headers=headers,params=param_string)
         response_content_dict = json.loads(response.content.decode("utf-8"))
         return dict(response_content_dict)
--- crust_break_recette_api/views.py
+++ crust_break_recette_api/views.py
@@ -7,4 +7,14 @@ def detailRecette(request,recette_id):
     return JsonResponse(Api().getRecipeInformations(recette_id))
 
 def searchRecette(request):
-    return
+    if request.GET.get('name') is not None or request.GET.get('name') is not '' :
+        query_dict = {
+            'name':request.GET.get('name'),#name of the food : burger, pizza...
+            'cuisine':request.GET.get('cuisine') if request.GET.get('cuisine') is not None else '',#name of the cuisine : american, african...
+            'type':request.GET.get('type') if request.GET.get('type') is not None else '',#type of meal : main course, entry...
+            'diet':request.GET.get('diet') if request.GET.get('diet') is not None else '',#type of diet : paleo, primal, and vegetarian...
+            'exclude':request.GET.get('exclude') if request.GET.get('exclude') is not None else ''
+        }
+        return JsonResponse(Api().searchRecipe(query_dict))
+    else:
+        return JsonResponse({'error':{'message':'missing the query string to search a recipe...'}})
