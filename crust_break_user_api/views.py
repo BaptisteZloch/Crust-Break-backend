94b3dd14ba940a905284500a010651b0ef87b435
Valentinemrct <valentine.mercent@gmail.com>
2022-04-12 18:32:29 +0200

    Test

--- crust_break_user_api/views.py
+++ crust_break_user_api/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+
+# Create your views here.
+
 
 # Create your views here.
 def addUser(request):
@@ -11,16 +15,28 @@ def updateUser(request):
     return
 def deleteUser(request):
     return
-def addRecetteToDo(request):
-    return
+
+def addRecetteToDo(request,id):
+        query = RecetteToDo.objects.all().get(id=receipe_id)
+        query.add()
+        return HttpResponse("Recette Ajoutée")
+    
+
 def addRecetteFavorites(request):
     return
-def deleteRecetteToDo(request):
-    return
+
+def deleteRecetteToDo(request, ID):
+    query = RecetteToDo.objects.all().get(ID=receipe_id)
+    query.delete()
+    return HttpResponse("Recette supprimée")
+    return render (request, 'deleteRecetteToDo.html', )
+   
 def deleteRecetteFavorites(request):
     return
-def getRecetteToDo(request):
-    return
+def getRecetteToDo(request,nom):
+    RecetteToDo.objects.all().get(nom=receipe_name).add()
+    return HttpResponse()
+
 def deleteRecetteFavorites(request):
     return
 def getRecetteFavorites(request):
