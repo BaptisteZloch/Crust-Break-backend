72d88d4e1bd4f25a986ebdf6e91afa05ac65c72c
Valentinemrct <valentine.mercent@gmail.com>
2022-04-13 13:05:49 +0200

    Add Recettes Favorites et Recettes ToDo

--- crust_break_user_api/views.py
+++ crust_break_user_api/views.py
@@ -1,4 +1,12 @@
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+import re
+import uuid
+from .api import *
+import based64 import decodestring
+import os
+from django.contrib.staticfiles.storage import staticfiles_storage
+
 
 # Create your views here.
 def addUser(request):
@@ -11,20 +19,40 @@ def updateUser(request):
     return
 def deleteUser(request):
     return
-def addRecetteToDo(request):
-    return
-def addRecetteFavorites(request):
-    return
+
+
+
+
+
+def addRecetteToDo(request, id):
+     query = Api().objects.all().get(id=receipe_id)
+     return JsonResponse(RecetteToDo.add(query))
+    #  si il n'y a pas de classe Recettes tout court chercher dans toute l'api
+    
+    
+def addRecetteFavorites(request, id):
+    query = Api().objects.all().get(id=receipe_id)
+    return JsonResponse(FavoritesReceipe.add(query))
+
+
 def deleteRecetteToDo(request):
     return
+
+
 def deleteRecetteFavorites(request):
     return
-def getRecetteToDo(request):
-    return
+
+def getRecetteToDo(request, receipe_id):
+    return JsonResponse(RecetteToDo().getRecipeInformations(receipe_id))
+
 def deleteRecetteFavorites(request):
     return
-def getRecetteFavorites(request):
-    return
+    
+def getRecetteFavorites(request, receipe_id):
+    return(RecetteFavorites().getRecipeInformations(receipe_id))
+
+
+
 def getRecettesRecommadations(request):
     return
 def generateListeCourses(request):
